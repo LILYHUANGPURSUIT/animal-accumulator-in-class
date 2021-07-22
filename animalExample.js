2ac9cc3779bae0830a5c545ef52c71b3ab066fd2
“Lili <lilyhuang114classb@gmail.com>
2021-07-22 15:59:38 -0400

    Finised exceises on 7/22/2021.

--- animalExample.js
+++ animalExample.js
@@ -1,5 +1,5 @@
-let animals = ["Wombat", "Lion", "Jaguar", "Dragon", "Anteater", "Capybara"];
-// // Combine all strings with comma and space
+// let animals = ["Wombat", "Lion", "Jaguar", "Dragon", "Anteater", "Capybara"];
+// // // Combine all strings with comma and space
 // function combineStr(animalArr){
 //     let animalsStr = "";
 //     for(let i=0; i<animalArr.length;i++){
@@ -11,6 +11,7 @@ let animals = ["Wombat", "Lion", "Jaguar", "Dragon", "Anteater", "Capybara"];
 //     return animalsStr;
 // }
 // console.log(combineStr(animals));
+
 
 // // Is an certain animal in the array?
 // function isAnimalInArr(animalArr, animalToFind){
@@ -34,24 +35,139 @@ const animalsObjArr = [
     { name: "Capybara", age: 3, species: "Hydrochoerinae"},
 ];
 
-// Combine all names of anmials into a string with comma and space between
+
+// Find oldest animal
+    // Return the age
+    // Return the animal    ---> return object
+    // function findOldOne(numbers) {
+    //         let oldestAge = 0;   //or let oldestAge = null;
+    //         for (let i=0; i<numbers.length;i++) {
+    //             let animalAge = numbers[i].age
+    //             if (animalAge > oldestAge) {
+    //                 oldestAge = animalAge;
+    //             };
+    //          }
+    //         return oldestAge;
+    //     }
+    //      console.log(findOldOne(animalsObjArr));
+
+// Return the animal    ---> return object
+// function findanimal(wholeInfor) {
+//     let animal = wholeInfor[0];
+//     for (let i=0; i<wholeInfor.length;i++) {
+//         let animalAge = wholeInfor[i].age
+//         if (animalAge > animal.age) {
+//             animal = wholeInfor[i];
+//         };
+//      }
+//     return animal;
+// }
+//  console.log(findanimal(animalsObjArr));
+
+// Find youngest animal
+    // Return the age
+// function findYoungOne(animalArr) {
+//     let yongestAge = animalArr[0].age;   
+//         for (let i=0; i<animalArr.length;i++) {
+//             let animalAge = animalArr[i].age
+//             if (animalAge < yongestAge) {
+//             yongestAge = animalAge;
+//             };
+//         }
+//     return yongestAge;
+// }
+// console.log(findYoungOne(animalsObjArr));
+    
+// Return the animal   --> Return an object
+// function findYoungestAnimal(wholeArr) {
+//     let newObj = wholeArr[0];
+//     for (let i=1; i<wholeArr.length;i++){
+//         let outPut = wholeArr[i].age
+//         if (outPut < newObj.age) {  
+//             newObj = wholeArr[i];
+//         }
+//     }
+
+//     return newObj;
+// }
+
+// console.log(findYoungestAnimal(animalsObjArr));
+
+    // // Combine all names of anmials into a string with comma and space between
+// function detailInfo(information) {
+//     let newDetailInfo = "";
+//     for (let i = 0; i < information.length; i++) {
+//         newDetailInfo += information[i].name;
+//         if (i !== information.length-1) {
+//             newDetailInfo += ", ";
+//         }
+//     }
+//     return newDetailInfo;
+// }
+// console.log(detailinfo(animalsObjArr));
 
 // Is a certain animal in the array?
     // Return true/false
     // Return the animal
+    // function findAnimal(allInfo, animal) {
+    //     let hasCertainAn = false;
+    //     for (let i = 0; i < allInfo.length; i++) {
+    //         let exactAnimal = allInfo[i].name;
+    //         if (exactAnimal === animal) {
+    //             hasCertainAn = true;
+    //         }
+    //     }
+    //     return hasCertainAn ;
+    // }
+    // console.log(findAnimal(animalsObjArr));
 
-// Filter array by number of letters in animal name
+    // function findAnimalObjInarr(animalArr, nameOfAnimal) {
+    //     let foundAnimalObj = {};
+    //     for (let i=0;i<animalArr.length;i++) {
+    //         let nameOfAnimalInArr = animalArr[i].name;
+    //         if (nameOfAnimal === nameOfAnimalInArr) {
+    //             foundAnimalObj = animalArr[i];
+    //         }
+    //     }
+    //     return foundAnimalObj;
+    // }
+    // console.log(findAnimalObjInarr(animalsObjArr),"Wombat");
+
+    // Filter array by number of letters in animal name
 // Example: Names longer than 5
+// function filterName(providedInfo, minNumOfChars) {
+//     let newArr = [];
+//     for (let i=0; i<providedInfo.length;i++) {
+//         let certainName = providedInfo[i].name;
+//         if (certainName.length > minNumOfChars) {
+//             newArr.push(providedInfo[i]);
+//         }
+//     }
+//     return newArr;
+// }
+// console.log(filterName(animalsObjArr, 5));
 
-// Filter animals older than given age
+//Filter animals older than given age
 // Example: Animals older than 4
+// function oldAnimals(originalInro,minAge) {
+//         let newArray = [];
+//         for (let i=0; i<originalInro.length;i++) {
+//             let newAge = originalInro[i].age;
+//             if (newAge > minAge) {
+//                 newArray.push(originalInro[i]);
+//             }
+//         }
+//         return newArray;
+//     }
+//     console.log(oldAnimals(animalsObjArr,4));
 
-// Find the average age of the animals
-
-// Find oldest animal
-    // Return the age
-    // Return the animal
-
-// Find youngest animal
-    // Return the age
-    // Return the animal
+//Find the average age of the animals
+// function calcuAge(inforNew) {
+//     let age = 0;
+//     for (let i=0; i<inforNew.length;i++) {
+//         let toAddAge = inforNew[i].age;
+//         age += toAddAge;
+//      }
+//     return age/inforNew.length;
+// }
+//  console.log(calcuAge(animalsObjArr));
